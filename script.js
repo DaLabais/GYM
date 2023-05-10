c0d92c5eaa258779886982050c636c5810f07103
DaLabais <114050986+DaLabais@users.noreply.github.com>
2023-05-10 09:37:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const coolButton = document.querySelector('.js-cool');
+coolButton.addEventListener('click', () =>{window.open ('https://www.youtube.com/watch?v=9t5G5XwDzmk')})
+
+
+console.log(coolButton)
